0510b54cf67a903449a8a0028023097767092610
Romlatao <thinhphatducpho2004@gmail.com>
2026-01-07 13:37:28 +0700

    refactor payment

--- paymentservice/src/main/java/com/se347/paymentservice/dtos/PaymentCompletedEvent.java
+++ paymentservice/src/main/java/com/se347/paymentservice/dtos/PaymentCompletedEvent.java
@@ -1,16 +1,27 @@
 package com.se347.paymentservice.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentCompletedEvent {
+    private UUID eventId;
+    private UUID paymentId;
     private UUID userId;
     private UUID courseId;
+    private UUID instructorId;
+    private BigDecimal amount;
     private String courseSlug;
+    private String vnpTxnRef;
+    private LocalDateTime completedAt;
+    private LocalDateTime occurredAt;
 }
